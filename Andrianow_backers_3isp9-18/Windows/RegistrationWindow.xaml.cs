823f5b8f735ec42303b6e52ad10c4d7a62709d5b
IMac3 <IMac3>
2023-02-09 14:02:44 +0300

    Доработал окна

--- Andrianow_backers_3isp9-18/Windows/RegistrationWindow.xaml.cs
+++ Andrianow_backers_3isp9-18/Windows/RegistrationWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -30,6 +31,102 @@ namespace Andrianow_backers_3isp9_18.Windows
             taskWindow.Show();
             Close();
         }
+        
+        //Работа с текст боксами регестрация
+
+        //ПОЛЕ NAME
+        private void txtRegName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtRegName.Text == "")
+            {
+                txtRegName.Text = "Name";
+            }
+            
+        }
+        private void txtRegName_GotFocus(object got, RoutedEventArgs a)
+        {
+            if (txtRegName.Text == "Name")
+            {
+                txtRegName.Text = "";
+            }
+               
+        }
+
+        //ПОЛЕ AGE
+        private void txtRegAge_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtRegAge.Text == "")
+            {
+                txtRegAge.Text = "Age";
+            }
+            
+        }
+        private void txtRegAge_GotFocus(object got, RoutedEventArgs a)
+        {
+            if (txtRegAge.Text == "Age")
+            {
+                txtRegAge.Text = "";
+            }
+            
+        }
+
+        //ПОЛЕ LOGIN
+        private void txtRegLogin_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtRegLogin.Text == "")
+            {
+                txtRegLogin.Text = "Login";
+            }
+            
+        }
+        private void txtRegLogin_GotFocus(object got, RoutedEventArgs a)
+        {
+            if (txtRegLogin.Text == "Login")
+            {
+                txtRegLogin.Text = "";
+            }
+          
+        }
+
+        //ПОЛЕ PASSWORD
+        private void txtRegPassword_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtRegPassword.Text == "")
+            {
+                txtRegPassword.Text = "Password";
+
+            }
+            
+        }
+        private void txtRegPassword_GotFocus(object got, RoutedEventArgs a)
+        {
+            if (txtRegPassword.Text == "Password")
+            {
+                txtRegPassword.Text = "";
+            }
+          
+        }
+
+        //ПОЛЕ PHONE
+
+        private void txtRegPhone_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtRegPhone.Text == "")
+            {
+                txtRegPhone.Text = "Phone";
+            }
+           
+        }
+        private void txtRegPhone_GotFocus(object got, RoutedEventArgs a)
+        {
+            if (txtRegPhone.Text == "Phone")
+            {
+                txtRegPhone.Text = "";
+            }
+            
+        }
+
+        
 
     }
 }
